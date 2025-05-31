622a632561bb7b6fcfcb352b1fca62e1940a91ad
ndip123 <nojong23@gmail.com>
2025-05-31 15:30:10 +0100

    Update frontend for env file of mongoatlas

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 // client/src/services/api.js
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5001/api';
+const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
 
 const api = axios.create({
     baseURL: API_URL,
