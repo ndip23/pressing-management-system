b80ea147ac29b21b0a28b1e80282ff4d2e338dd9
ndip123 <nojong23@gmail.com>
2025-06-03 20:32:49 +0100

    update for cors prob

--- backend/server.js
+++ backend/server.js
@@ -60,5 +60,3 @@ app.use(errorHandler);
 // (which you are not, as your backend is on Render), you would export 'app'.
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
-
- export default app;
