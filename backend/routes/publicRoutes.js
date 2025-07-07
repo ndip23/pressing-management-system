06d6ff2004678bc3d722ca78eeaf02536ba8e62a
ndip123 <nojong23@gmail.com>
2025-07-07 07:19:59 +0100

    for otp err

--- backend/routes/publicRoutes.js
+++ backend/routes/publicRoutes.js
@@ -1,8 +1,7 @@
 // server/routes/publicRoutes.js
 import express from 'express';
-import { registerTenantWithSetup, initiateRegistration, finalizeRegistration } from '../controllers/publicController.js';
+import {initiateRegistration, finalizeRegistration } from '../controllers/publicController.js';
 const router = express.Router();
-router.post('/register-with-setup', registerTenantWithSetup);
 router.post('/initiate-registration', initiateRegistration); // Step 1
 router.post('/finalize-registration', finalizeRegistration);
 export default router;
