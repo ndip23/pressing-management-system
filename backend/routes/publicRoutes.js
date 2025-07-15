9c0ff9d148dc554dd9231df8aeaddde0a1ef240f
ndip123 <nojong23@gmail.com>
2025-07-15 15:55:42 +0100

    for err on directives

--- backend/routes/publicRoutes.js
+++ backend/routes/publicRoutes.js
@@ -1,6 +1,6 @@
 // server/routes/publicRoutes.js
 import express from 'express';
-import {initiateRegistration, finalizeRegistration } from '../controllers/publicController.js';
+import {initiateRegistration, finalizeRegistration, getPublicDirectory } from '../controllers/publicController.js';
 const router = express.Router();
 router.post('/initiate-registration', initiateRegistration); // Step 1
 router.post('/finalize-registration', finalizeRegistration);
